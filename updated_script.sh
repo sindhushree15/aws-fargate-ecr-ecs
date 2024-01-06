e8a89f6c3f6c96beb7a3475c3abaaabc56762a6b
sindhushree15 <sindhushree.15@gmail.com>
2024-01-06 19:27:11 +0530

    Update updated_script.sh

--- updated_script.sh
+++ updated_script.sh
@@ -20,6 +20,38 @@ aws ecs register-task-definition --cli-input-json file://task-definition.json --
 
  REVISION=`aws ecs describe-task-definition --task-definition jenkin-streamlit --region ca-central-1 | jq .taskDefinition.revision`
  echo "REVISION= " "${REVISION}"
+
+ #Check wheather load balancers exists 
+BLNCR_ARN=`aws elbv2 describe-load-balancers --load-balancer-arns arn:aws:elasticloadbalancing:ca-central-1:816605281523:loadbalancer/app/secure-applications-ecs/d9db12d799161a4c --region ca-central-1 | jq.LoadBalancers[].LoadBalancerArn`
+
+if [ -z "$BLNCR_ARN" ]; then
+    echo "🔥🔥🔥🔥🔥Balancer doesn't exists, create one and save the ARN🔥🔥🔥🔥🔥🔥"
+    BLNCR_ARN=`aws elbv2 create-load-balancer \
+     --scheme internal \
+     --name my-testing-balancer \
+     --subnets subnet-0da689ba36c2af4c9 subnet-0875527f8fb822536 \
+     --region ca-central-1 \
+     --security-groups sg-0c107ac1969ee10a4 | jq.LoadBalancers[].LoadBalancerArn`
+
+     TARGET_GRP_ARN=`aws elbv2 create-target-group --name jenkins-target --protocol HTTP --port 80 \
+     --vpc-id vpc-013a94a651e62b40b --ip-address-type ipv4 --target-type ip \
+     --region ca-central-1 | jq .TargetGroups[].TargetGroupArn`
+
+     aws elbv2 register-targets --target-group-arn $TARGET_GRP_ARN  \
+     --targets Id=10.211.29.189 \
+     --region ca-central-1
+
+
+     aws elbv2 create-listener --load-balancer-arn $BLNCR_ARN \
+     --protocol HTTP --port 80  \
+     --default-actions Type=forward,TargetGroupArn=$TARGET_GRP_ARN \
+     --region ca-central-1
+     
+     
+else
+    echo "🔥🔥🔥🔥🔥$BLNCR_ARN🔥🔥🔥🔥🔥🔥"
+fi
+
 # aws ecs update-service --cluster ECSFargateForPipeline --service jenkin-streamlit --task-definition jenkin-streamlit-service --desired-count 1 --region ca-central-1
 aws ecs update-service --cluster ECSFargateForPipeline --service jenkin-streamlit-service --task-definition jenkin-streamlit --region ca-central-1 --desired-count 2
 
